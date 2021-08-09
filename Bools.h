293e26d45d59130b8f4e605c3ac9bd39ca2a2c7a
T0nd0Tara <5632815@gmail.com>
2021-08-09 10:30:02 +0300

    Bools::str + moving methods to outside of class. Bools copy constructer & copy assigner

--- Bools.h
+++ Bools.h
@@ -10,54 +10,50 @@ namespace tara {
     struct Bools {
         uint8_t data = 0x00;
 
-        bool get_val(int index) {
-            return (data >> modulu(index, 8)) & 1;
-        }
-        void inv_val(int index) {
-            data ^= 1 << modulu(index, 8);
-        }
+    public:
+        Bools(uint8_t set_vals = 0x00);
 
-        void set_val(int index, bool val) {
-            if (get_val(index) != val)
-                inv_val(index);
-        }
+        Bools(const Bools&);
 
-        bool operator[](int index) {
-            //return (data>>index) & 1;
-            return get_val(index);
-        }
+        bool get_val(int);
+        void inv_val(int);
 
-        friend std::ostream& operator<<(std::ostream& out, Bools vals) {
-            out << vals[7]
-                << vals[6]
-                << vals[5]
-                << vals[4]
-                << vals[3]
-                << vals[2]
-                << vals[1]
-                << vals[0];
-            return out;
-        }
+        void set_val(int, bool);
 
-        friend Bools operator^(Bools b1, Bools b2) {
-            return Bools(b1.data ^ b2.data);
-        }
+        
 
-        friend Bools operator&(Bools b1, Bools b2) {
-            return Bools(b1.data & b2.data);
-        }
+        std::string str();
 
-        friend Bools operator|(Bools b1, Bools b2) {
-            return Bools(b1.data | b2.data);
-        }
+        void reset(int index) { set_val(index, false); }
 
-        friend Bools operator<<(Bools b, int n) {
-            return Bools(b.data << n);
-        }
+        void off(int index) { set_val(index, false); }
 
-        friend Bools operator>>(Bools b, int n) {
-            return Bools(b.data >> n);
-        }
+        void on(int index) { set_val(index, true); }
+
+        void flip(int index) { inv_val(index); }
+
+
+        std::vector<uint8_t>& get_on();
+
+        std::vector<uint8_t>& get_off();
+
+        bool operator[](int);
+
+        Bools& operator=(uint8_t);
+        Bools& operator=(Bools);
+
+        friend std::ostream& operator<<(std::ostream&, Bools);
+        
+
+        friend Bools operator^(Bools, Bools);
+
+        friend Bools operator&(Bools, Bools);
+
+        friend Bools operator|(Bools, Bools);
+
+        friend Bools operator<<(Bools, uint8_t);
+
+        friend Bools operator>>(Bools, uint8_t);
 
 
         // friend bools operator^(bools b1, char n){
@@ -72,59 +68,114 @@ namespace tara {
         //     return bools(b1.data | n);
         // }
 
-
-
-        void reset(int index) { set_val(index, false); }
-
-        void off(int index) { set_val(index, false); }
-
-        void on(int index) { set_val(index, true); }
-
-        void flip(int index) { inv_val(index); }
-
-
-        std::vector<uint8_t> get_on() {
-            std::vector<uint8_t> out;
-
-            for (uint8_t i = 0; i < 8; i++)
-                if (get_val(i))
-                    out.push_back(i);
-
-
-            return out;
-        }
-
-        std::vector<uint8_t> get_off() {
-            std::vector<uint8_t> out;
-
-            for (uint8_t i = 0; i < 8; i++)
-                if (!get_val(i))
-                    out.push_back(i);
-
-
-            return out;
-        }
-
-        Bools(uint8_t set_vals = 0x00) {
-            data = set_vals;
-        }
+        
     };
+    // ========================Constructors========================
+#pragma region Constructors
+    Bools::Bools(uint8_t set_vals) {
+        data = set_vals;
+    }
 
+    Bools::Bools(const Bools& rhs) {
+        data = rhs.data;
+    }
+
+#pragma endregion Constructors
+
+    // ==========================Methods===========================
+#pragma region Methods
+    bool Bools::get_val(int index) {
+        return (data >> modulu(index, 8)) & 1;
+    }
+
+    void Bools::inv_val(int index) {
+        data ^= 1 << modulu(index, 8);
+    }
+
+    void Bools::set_val(int index, bool val) {
+        if (get_val(index) != val)
+            inv_val(index);
+    }
+
+    std::string Bools::str() {
+        std::string out =
+            btos(get_val(7))
+            + btos(get_val(6))
+            + btos(get_val(5))
+            + btos(get_val(4))
+            + btos(get_val(3))
+            + btos(get_val(2))
+            + btos(get_val(1))
+            + btos(get_val(0));
+        return out;
+    }
+
+    std::vector<uint8_t>& Bools::get_on() {
+        std::vector<uint8_t> out;
+
+        for (uint8_t i = 0; i < 8; i++)
+            if (get_val(i))
+                out.push_back(i);
+
+
+        return out;
+    }
+
+    std::vector<uint8_t>& Bools::get_off() {
+        std::vector<uint8_t> out;
+
+        for (uint8_t i = 0; i < 8; i++)
+            if (!get_val(i))
+                out.push_back(i);
+
+
+        return out;
+    }
     
+#pragma endregion Methods
+
+    //	========================Operators==========================
+#pragma region Operators
+    bool Bools::operator[](int index) {
+        return get_val(index);
+    }
+
+    Bools& Bools::operator=(uint8_t setValue) {
+        data = setValue;
+        return *this;
+    }
+
+    Bools& Bools::operator=(Bools rhs) {
+        data = rhs.data;
+        return *this;
+    }
+
+    std::ostream& operator<<(std::ostream& out, Bools vals) {
+        out << vals.str();
+        return out;
+    }
+
+    Bools operator^(Bools b1, Bools b2) {
+        return Bools(b1.data ^ b2.data);
+    }
+
+    Bools operator&(Bools b1, Bools b2) {
+        return Bools(b1.data & b2.data);
+    }
+
+    Bools operator|(Bools b1, Bools b2) {
+        return Bools(b1.data | b2.data);
+    }
+
+    Bools operator<<(Bools b, uint8_t n) {
+        return Bools(b.data << n);
+    }
+
+    Bools operator>>(Bools b, uint8_t n) {
+        return Bools(b.data >> n);
+    }
+#pragma endregion Operators
 }
 
 
 
-std::string to_str(tara::Bools in) {
-    std::string out;
-    out += btos(in[7]);
-    out += btos(in[6]);
-    out += btos(in[5]);
-    out += btos(in[4]);
-    out += btos(in[3]);
-    out += btos(in[2]);
-    out += btos(in[1]);
-    out += btos(in[0]);
-
-    return out;
-}
