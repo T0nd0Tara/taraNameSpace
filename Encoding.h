5f3f1f8f669fd5cb4049d66e565536511fcf0f9a
T0nd0Tara <5632815@gmail.com>
2021-10-02 17:13:04 +0300

    Encoding.h: +Expanding word32, *making word32 5 bytes

--- Encoding.h
+++ Encoding.h
@@ -111,64 +111,186 @@ namespace tara {
     // every char is in base 32
     class word32 {
     protected:
-        uint32_t main;
-        uint8_t  e;
+        /*
+        cell          :    7           6              5            4           3             2             1           0
+        representation:_ _ _ _ _ | _ _ _ ~ _ _ | _ _ _ _ _ | _ ~ _ _ _ _ | _ _ _ _ ~ _ | _ _ _ _ _ | _ _ ~ _ _ _ | _ _ _ _ _
+        container     :e---------------e   d-----------------d   c---------------c   b-----------------b   a---------------a
+        */
+        uint8_t e, d, c, b, a;
 
-        void ConstructByStr(std::string in) {
-            if (in.length() > 8)
-                in = in.substr(0U, 8);
-            else while (in.length() < 8) {
-                in.insert(in.begin(), '0');
-            }
-
-
-            e = decoder32.at(in[0]) << 3;
-            e |= decoder32.at(in[1]) >> 2;
-
-            main = decoder32.at(in[1]) << 30;
-            main |= decoder32.at(in[2]) << 25;
-            main |= decoder32.at(in[3]) << 20;
-            main |= decoder32.at(in[4]) << 15;
-            main |= decoder32.at(in[5]) << 10;
-            main |= decoder32.at(in[6]) << 5;
-            main |= decoder32.at(in[7]);
-        }
-
+        void ConstructByStr(std::string);
     public:
-        word32() {
-            main = 0U;
-            e = 0U;
-        }
+        void set_char(uint8_t val, uint8_t index);
+    public:
+        word32();
+        word32(word32);
+        word32(std::string in);
+        word32(uint32_t in);
 
-        word32(std::string in) {
-            ConstructByStr(in);
-        }
+        uint8_t get_char(uint8_t index);
 
-        word32(uint32_t in) {
-            ConstructByStr(encoding32(in));
-        }
+        std::string to_str();
+        const char* c_str();
 
-        std::string to_str() {
-            std::string out =
-                      std::string(1, encoder32.at(e >> 3))
-                    + std::string(1, encoder32.at(((e & 0b111) << 2) | (main >> 30)))
-                    + std::string(1, encoder32.at((main >> 25) & 0x1F)) // 0x1F == 0b11111
-                    + std::string(1, encoder32.at((main >> 20) & 0x1F))
-                    + std::string(1, encoder32.at((main >> 15) & 0x1F))
-                    + std::string(1, encoder32.at((main >> 10) & 0x1F))
-                    + std::string(1, encoder32.at((main >> 5)  & 0x1F))
-                    + std::string(1, encoder32.at((main >> 0)  & 0x1F));
+        word32& operator=(word32);
+        word32& operator=(std::string);
+        word32& operator=(uint32_t);
 
-
+        friend std::ostream& operator<<(std::ostream& out, word32 a) {
+            out << a.to_str();
             return out;
         }
 
-        const char* c_str() {
-            return to_str().c_str();
+    };
+    // ========================Constructors========================
+#pragma region Constructers
+    word32::word32() {
+        a = 0U;
+        b = 0U;
+        c = 0U;
+        d = 0U;
+        e = 0U;
+    }
+
+    word32::word32(word32 in) {
+        a = in.a;
+        b = in.b;
+        c = in.c;
+        d = in.d;
+        e = in.e;
+    }
+    word32::word32(std::string in) {
+        ConstructByStr(in);
+    }
+
+    word32::word32(uint32_t in) {
+        ConstructByStr(encoding32(in));
+    }
+#pragma endregion
+    // ==========================Methods===========================
+#pragma region Methods
+    void word32::ConstructByStr(std::string in) {
+        if (in.length() > 8)
+            in = in.substr(0U, 8);
+        else while (in.length() < 8) {
+            in.insert(in.begin(), '0');
         }
 
 
+        set_char(decoder32.at(in[0]), 7);
+        set_char(decoder32.at(in[1]), 6);
+        set_char(decoder32.at(in[2]), 5);
+        set_char(decoder32.at(in[3]), 4);
+        set_char(decoder32.at(in[4]), 3);
+        set_char(decoder32.at(in[5]), 2);
+        set_char(decoder32.at(in[6]), 1);
+        set_char(decoder32.at(in[7]), 0);
 
-    };
+    }
+
+    void word32::set_char(uint8_t val, uint8_t index) {
+        val = (uint8_t)modulu((uint16_t)val, (uint16_t)32);
+        switch (modulu((uint8_t)index, (uint8_t)8)) {
+        case 0:
+            a = (a & 0xE0) | val;
+            return;
+        case 1:
+            a = (a & 0x1F) | (val << 5);
+            b = (b & 0xFC) | (val >> 3);
+            return;
+        case 2:
+            b = (b & 0x83) | (val << 2);
+            return;
+        case 3:
+            b = (b & 0x7F) | (val << 7);
+            c = (c & 0xF0) | (val >> 1);
+            return;
+        case 4:
+            c = (c & 0xF) | (val << 4);
+            d = (d & 0xFE) | (val >> 4);
+            return;
+        case 5:
+            d = (d & 0xC1) | (val << 1);
+            return;
+        case 6:
+            d = (d & 0x3F) | (val << 6);
+            e = (e & 0xF8) | (val >> 2);
+            return;
+        case 7:
+            e = (e & 0x7) | (val << 3);
+            return;
+        }
+
+        printError("modulu returned value "
+            + std::to_string(modulu((uint8_t)index, (uint8_t)8))
+            + ", which is not between 0 and 7.\nPlease contact develepors if you encounter this error.\n");
+    }
+
+    uint8_t word32::get_char(uint8_t index) {
+        switch (modulu((uint8_t)index, (uint8_t)8)) {
+        case 0:
+            return a & 0x1F;
+        case 1:
+            return (a >> 5) | ((b & 3) << 3);
+        case 2:
+            return (b >> 2) & 0x1F;
+        case 3:
+            return (b >> 7) | ((c & 0xF) << 1);
+        case 4:
+            return (c >> 4) | ((d & 1) << 4);
+        case 5:
+            return (d >> 1) & 0x1F;
+        case 6:
+            return (d >> 6) | ((e & 0b111) << 2);
+        case 7:
+            return e >> 3;
+        }
+        printError("modulu returned value "
+            + std::to_string(modulu((uint8_t)index, (uint8_t)8))
+            + ", which is not between 0 and 7.\nPlease contact develepors if you encounter this error.\n");
+
+        // to not have a warning :b
+        return 0;
+    }
+
+    std::string word32::to_str() {
+        std::string out =
+            std::string(1, encoder32.at(get_char(7)))
+            + std::string(1, encoder32.at(get_char(6)))
+            + std::string(1, encoder32.at(get_char(5)))
+            + std::string(1, encoder32.at(get_char(4)))
+            + std::string(1, encoder32.at(get_char(3)))
+            + std::string(1, encoder32.at(get_char(2)))
+            + std::string(1, encoder32.at(get_char(1)))
+            + std::string(1, encoder32.at(get_char(0)));
+
+        return out;
+    }
+
+    const char* word32::c_str() {
+        return to_str().c_str();
+    }
 #pragma endregion
+
+    // =========================Operators==========================
+#pragma region Operators
+    word32& word32::operator=(word32 in) {
+        a = in.a;
+        b = in.b;
+        c = in.c;
+        d = in.d;
+        e = in.e;
+        return *this;
+    }
+    word32& word32::operator=(std::string in) {
+        ConstructByStr(in);
+        return *this;
+    }
+
+    word32& word32::operator=(uint32_t in) {
+        ConstructByStr(encoding32(in));
+        return *this;
+    }
+#pragma endregion
+#pragma endregion Crockfords Base32
 }
